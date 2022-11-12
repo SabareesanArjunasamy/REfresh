1f6c13984a14882ea4497905fc5df9f86a80dc0d
Sabareesan Arjunasamy <sab@ajirasoft.com>
2022-11-13 02:11:23 +0530

    add py file to find pivote index in a array

---
+++ findPivotIndex.py
@@ -0,0 +1,9 @@
+def pivotIndex(nums):
+    totalValue = sum(nums)
+    leftSumValue = 0
+    for index, element in enumerate(nums):
+        if leftSumValue == (totalValue - nums[index] - leftSumValue):
+            return index
+        else:
+            leftSumValue += element
+    return -1
